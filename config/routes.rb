edf13ed08d9b1fc5b5e00f3c6645c01722551957
3627839 <openap@gmail.com>
2019-02-16 01:25:40 -0300

    Add Check new versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/index'
+  get 'index', to:'pages#index'
+  get 'pages/home'
+
+  get 'pages/show'
+
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
